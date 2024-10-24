3287da876ed55c57a003e39a6d7deae32ebbe441
francorl <68926990+francorl@users.noreply.github.com>
2024-10-24 02:34:04 +0200

    First Commit
    First Commit

---
+++ Shootemup/Code/CharacterMovement.gd
@@ -0,0 +1,105 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+@export var Bullet: PackedScene
+@onready var Camera = get_node("Camera2D")
+
+@export var joystick_left: VirtualJoystick
+@export var joystick_right: VirtualJoystick
+@export var speed: float = 100
+@export var fire_rate = 0.2
+var actual_rate = 0.2
+var timer = 0
+
+var power = false
+var power_timer = 0
+
+@onready var absolute_parent = get_parent()
+
+# Controla si el jugador está muerto.
+var die: bool = false
+
+#func _ready():
+	 #Camera.set("position", Vector2(100, 0))
+
+var move_vector := Vector2.ZERO
+
+func _process(delta: float) -> void:
+	move_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += move_vector * speed * delta
+
+	# Rotación utilizando el joystick derecho si está activo.
+	if joystick_right and joystick_right.is_pressed:
+		rotation = joystick_right.output.angle()
+
+func _physics_process(delta):
+	timer += delta
+
+	if power == true:
+		power_timer += delta
+		actual_rate = fire_rate / 2
+		if power_timer >= 10:
+			power = false
+	else:
+		actual_rate = fire_rate
+		power_timer = 0
+
+	velocity = Vector2.ZERO
+
+	if die == true:
+		if Input.get_action_raw_strength("Respawn"):
+			Respawn()
+		return
+
+	# Disparo.
+	if timer >= actual_rate:
+		var temp = Bullet.instantiate()
+		add_sibling(temp)
+		temp.global_position = get_node("BulletSpawn").global_position
+		
+		# Determinar la dirección de disparo usando joystick o ratón.
+		var shoot_direction = Vector2.ZERO
+		if joystick_right and joystick_right.is_pressed:
+			shoot_direction = joystick_right.output.normalized()
+		else:
+			shoot_direction = (get_global_mouse_position() - global_position).normalized()
+		
+		temp.set("area_direction", shoot_direction)
+		
+		# Rotar el jugador hacia la dirección de disparo.
+		if joystick_right and joystick_right.is_pressed:
+			rotation = joystick_right.output.angle()
+		else:
+			look_at(get_global_mouse_position())
+
+		# Efecto de sacudida de cámara.
+		Camera.offset = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+		timer = 0
+	else:
+		Camera.offset = Vector2(0, 0)
+
+	# Movimiento.
+	var direction_x = Input.get_axis("Left", "Right")
+	var direction_y = Input.get_axis("Up", "Down")
+	
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	if direction_y:
+		velocity.y = direction_y * SPEED
+
+	move_and_slide()
+
+func Die():
+	get_node("Explosive").set_emitting(true)
+	get_node("Explosive/Sound").play()
+	get_node("MeshInstance2D").visible = false
+	Camera.position = Vector2(0, 0)
+	die = true
+	await get_tree().create_timer(1.5).timeout
+	position = Vector2(383, 397)
+	joystick_left.visible = false
+	joystick_right.visible = false
+	$"../Retry".show()
+
+func Respawn():
+	get_tree().reload_current_scene()
