fe7c4677680d2cb4f0c331953317354e02b70082
Fran <francoorlando11@gmail.com>
2024-11-10 18:32:05 +0100

    Refactor
    Creacion de carpetas
    Cambio de fuente
    Cambio de nombres

--- Shootemup/Code/menu.gd
+++ Shootemup/Code/menu.gd
@@ -8,10 +8,10 @@ func _ready():
 	#StarsBackground.get_node("StarFieldMid").emmiting = true
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file ("res://Scene/Jugar.tscn")
+	get_tree().change_scene_to_file ("res://Scene/UI/Play.tscn")
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scene/OPTIONS.tscn")
+	get_tree().change_scene_to_file("res://Scene/UI/Options.tscn")
 
 
 func _on_exit_pressed() -> void:
